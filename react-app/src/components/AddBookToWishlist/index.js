be58d57cfb21e6418ce7fd062d388e24dc162297
Jeanette <haltonjeanette@gmail.com>
2023-10-21 22:06:03 -0700

    Begin AddBookToWishlistModal

---
+++ react-app/src/components/AddBookToWishlist/index.js
@@ -0,0 +1,12 @@
+import "./addBookToWishlist.css";
+import React from "react";
+
+const AddBookToWishlistModal = () => {
+  return (
+    <div>
+      <h1>Add Book to Wishlist Modal Component</h1>
+    </div>
+  );
+};
+
+export default AddBookToWishlistModal;
--- react-app/src/components/WishlistDetail/index.js
+++ react-app/src/components/WishlistDetail/index.js
@@ -1,7 +1,24 @@
+import { useEffect } from "react";
 import "./WishlistDetail.css";
 import React from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { currentWishlist, getWishlistById } from "../../store/wishlists";
+import LoadingSpinner from "../LoadingSpinner";
 
 const WishlistDetail = () => {
+  const { wishlistId } = useParams();
+  const dispatch = useDispatch();
+  const wishlist = useSelector(currentWishlist);
+
+  useEffect(() => {
+    dispatch(getWishlistById(wishlistId));
+  }, [dispatch, wishlistId]);
+
+  if (!wishlist) {
+    return <LoadingSpinner />;
+  }
+
   return (
     <div>
       <h1>Wishlist Detail Component</h1>
